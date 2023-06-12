70ddc337de0a67b9c4a58603b6ac03ec43809fce
193499-Hồ Hoàng Việt Tiến <tien.hohoangviet@thegioididong.com>
2023-06-12 15:24:39 +0700

    update chat messenger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,28 @@ export default function RootLayout({
           <Footer />
           <BackgroundVideo />
         </main>
+        <Script id="fb-messenger" strategy="lazyOnload">
+          {`
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "105241572603864");
+            chatbox.setAttribute("attribution", "biz_inbox");
+      
+            window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml            : true,
+                  version          : 'v17.0'
+                });
+              };
+        
+              (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
+        `}
+        </Script>
       </body>
     </html>
   );
